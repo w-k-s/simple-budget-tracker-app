5e60b95356a6804ea744293c378760580bab8491
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2021-09-18 16:45:50 +0400

    Group Records

--- lib/screens/records_screen.dart
+++ lib/screens/records_screen.dart
@@ -16,7 +16,7 @@ class RecordsScreen extends StatefulWidget {
 class _RecordsScreenState extends State<RecordsScreen> {
   bool isLoading = false;
   String? error;
-  Records? records;
+  RecordsViewModel? records;
   Accounts? accounts;
   Account? selectedAccount;
 
@@ -63,7 +63,7 @@ class _RecordsScreenState extends State<RecordsScreen> {
         .onSuccess((json) => Records.fromJson(json))
         .then((_records) {
       setState(() {
-        records = _records;
+        records = RecordsViewModel.fromRecords(_records);
       });
     }).catchError((e) {
       setState(() {
@@ -127,7 +127,7 @@ class _RecordsScreenState extends State<RecordsScreen> {
     }
 
     final _records = records;
-    if (_records == null || _records.records.isEmpty) {
+    if (_records == null || _records.isEmpty) {
       return Center(
         child: Text(
           "No records",
@@ -137,27 +137,15 @@ class _RecordsScreenState extends State<RecordsScreen> {
       );
     }
 
-    return _buildRecordsList(context, _records);
+    return _buildRecordsList(context);
   }
 
-  ListView _buildRecordsList(BuildContext context, Records records) {
+  ListView _buildRecordsList(BuildContext context) {
     return ListView.builder(
-      itemCount: records.records.length,
+      itemCount: records?.items.length ?? 0,
       padding: EdgeInsets.all(8),
       itemBuilder: (context, index) {
-        final record = records.records[index];
-        return Card(
-          elevation: 4,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(record.note),
-              subtitle: Text(
-                  "${new DateFormat("yyyy-MMM-dd").format(record.dateUTC.toLocal())} • ${record.category.name}"),
-              trailing: Text(record.amount.toString()),
-            ),
-          ),
-        );
+        return records!.items[index].buildTile(context);
       },
     );
   }
@@ -170,3 +158,143 @@ class _RecordsScreenState extends State<RecordsScreen> {
         .whenComplete(() => _setLoading(false)));
   }
 }
+
+abstract class RecordListTile {
+  Widget buildTile(BuildContext context);
+}
+
+class CalendarMonthListItem implements RecordListTile {
+  static final dateFormat = DateFormat("MMMM yyyy");
+  final CalendarMonth calendarMonth;
+
+  CalendarMonthListItem({required this.calendarMonth});
+
+  @override
+  Widget buildTile(BuildContext context) {
+    return Text(
+      dateFormat.format(calendarMonth.month.toLocal()),
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+}
+
+class DateListItem implements RecordListTile {
+  static final dateFormat = DateFormat("EEEE, dd");
+
+  final DateTime date;
+  DateListItem({required this.date});
+
+  @override
+  Widget buildTile(BuildContext context) {
+    return Text(
+      dateFormat.format(date.toLocal()),
+      style: Theme.of(context).textTheme.headline6,
+    );
+  }
+}
+
+class RecordItem implements RecordListTile {
+  final Record record;
+  RecordItem({required this.record});
+
+  @override
+  Widget buildTile(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          title: Text(record.note),
+          subtitle: Text(
+              "${new DateFormat("yyyy-MMM-dd").format(record.dateUTC.toLocal())} • ${record.category.name}"),
+          trailing: Text(record.amount.toString()),
+        ),
+      ),
+    );
+  }
+}
+
+class CalendarMonth {
+  DateTime month;
+  CalendarMonth({required this.month});
+  factory CalendarMonth.of(int month, int year) =>
+      CalendarMonth(month: DateTime.utc(year, month, 1));
+  factory CalendarMonth.ofDate(DateTime date) =>
+      CalendarMonth.of(date.month, date.year);
+
+  @override
+  operator ==(o) => o is CalendarMonth && o.month == month;
+
+  @override
+  int get hashCode => month.hashCode;
+
+  @override
+  String toString() {
+    return month.toString();
+  }
+}
+
+class RecordsViewModel {
+  List<RecordListTile> items;
+
+  RecordsViewModel({required this.items});
+
+  factory RecordsViewModel.fromRecords(Records records) {
+    final recordsList = records.records;
+
+    if (recordsList.isEmpty) {
+      return RecordsViewModel(items: []);
+    }
+
+    recordsList.sort((r1, r2) {
+      if (r1.dateUTC == r2.dateUTC) return 0;
+      if (r2.dateUTC.isAfter(r1.dateUTC)) return 1;
+      return -1;
+    });
+
+    final expensesPerMonth = Map<CalendarMonth, DailyExpenses>();
+    for (final record in recordsList) {
+      final recordDate = DateTime.utc(
+          record.dateUTC.year, record.dateUTC.month, record.dateUTC.day);
+
+      final calendarMonth = CalendarMonth.ofDate(record.dateUTC);
+      final dailyExpenses = expensesPerMonth[calendarMonth];
+      if (dailyExpenses == null) {
+        expensesPerMonth[calendarMonth] = Map<DateTime, List<Record>>();
+        expensesPerMonth[calendarMonth]![recordDate] = [record];
+        continue;
+      }
+
+      final expensesOfDay = dailyExpenses[recordDate];
+      if (expensesOfDay == null) {
+        dailyExpenses[recordDate] = [record];
+      } else {
+        expensesOfDay.add(record);
+      }
+    }
+
+    debugPrint(expensesPerMonth.toString());
+    final items = <RecordListTile>[];
+    for (final monthlyEntry in expensesPerMonth.entries) {
+      final month = monthlyEntry.key;
+      final expensesPerDay = monthlyEntry.value;
+
+      items.add(CalendarMonthListItem(calendarMonth: month));
+      for (final dailyEntry in expensesPerDay.entries) {
+        final day = dailyEntry.key;
+        final records = dailyEntry.value;
+
+        items.add(DateListItem(date: dailyEntry.key));
+        for (final record in records) {
+          items.add(RecordItem(record: record));
+        }
+      }
+    }
+
+    return RecordsViewModel(items: items);
+  }
+
+  bool get isEmpty => this.items.isEmpty;
+}
+
+typedef DailyExpenses = Map<DateTime, List<Record>>;
